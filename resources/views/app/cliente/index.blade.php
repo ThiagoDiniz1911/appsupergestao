857498774d30d4a3b6670035665bcf8316c45037
Thiago Diniz <thiagodiniz1911@gmail.com>
2024-04-29 22:12:04 -0300

    App Super Gestão1

--- resources/views/app/cliente/index.blade.php
+++ resources/views/app/cliente/index.blade.php
@@ -15,7 +15,7 @@
     <div class="menu">
         <ul>
             <li><a href="{{route('cliente.create')}}">Novo</a></li>
-            <li><a href="">Consulta</a></li>
+            <li><a href="">Consultas</a></li>
         </ul>
     </div>
 
